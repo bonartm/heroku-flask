1747ae8a70b40b8190a4e3b6deeac43756561e20
bonartm <malte@spiced-academy.com>
2020-04-21 16:20:40 +0200

    update app

--- gettingstarted/app.py
+++ gettingstarted/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:#10397a'>Hello There!</h1>"
+
+@app.route('/') 
+@app.route('/index')
+def hello_world():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
